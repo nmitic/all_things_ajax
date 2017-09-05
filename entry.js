7805cc1ee3a0ef747d355aaa92b17dfb7bd8b189
Nikola Mitic <nikola.mitic@namics.com>
2017-09-05 21:51:12 +0200

    initial commit

---
+++ entry.js
@@ -0,0 +1,6 @@
+ document.write(require("./content.js"));
+
+let ime = 'nikola',
+    godine = 26;
+
+console.log(`Moje ime je ${ime} i imam ${godine} godina!`)
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+let LiveReloadPlugin = require('webpack-livereload-plugin');
+
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new LiveReloadPlugin()
+    ]
+};
